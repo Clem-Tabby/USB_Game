0f60b0095f2c70806f378a69d64c3c9e5dc25c1c
Clem-Tabby <thenicholasfarrell@gmail.com>
2022-03-14 16:07:12 -0400

    Coins can now be collected, score added and increments with coin collection

--- gameboard.py
+++ gameboard.py
@@ -5,12 +5,13 @@ from player import Player
 from settings import screen_height, screen_width, coin_spawn_percent, \
     background_color
 import random
-from sounds import sfx, bump
+from sounds import sfx, bump, bling
 
 class Gameboard:
 
     def __init__(self, surface):
         self.surface = surface
+        self.score = 0
 
         # Define sprite groups
         self.coins = pygame.sprite.Group()
@@ -26,13 +27,29 @@ class Gameboard:
             self.my_joystick = pygame.joystick.Joystick(0)
             self.my_joystick.init()
 
+    def draw_score(self, surface):
+        score_string = 'SCORE: ' + str(self.score)
+        score_font = pygame.font.Font('freesansbold.ttf', 25)
+        score_text = score_font.render(score_string, True, (0, 0, 0))
+        text_width, text_height = score_font.size(score_string)
+        surface.blit(score_text, (10,10))
+
     # spawns coins randomly along right edge of board,
     # spawn_percent defines percentage chance that a coin spawns
-    # per tick of the game clock,
+    # per tick of the game clock
     def spawn_coins(self, coin_spawn_percent):
         if random.uniform(0, 100) <= coin_spawn_percent:
             coin = Coin((screen_width, int(random.uniform(0, screen_height - 16))))
             self.coins.add(coin)
+
+    def check_coin_collision(self):
+        for coin in self.coins.sprites():
+            if self.player1.rect.colliderect(coin.rect):
+                coin.kill()
+                sfx.play(bling)
+                self.score += 100
+            elif coin.rect.x < -coin.rect.width:
+                coin.kill()
 
     def run(self):
         if self.joystick_count != 0:
@@ -40,8 +57,10 @@ class Gameboard:
             vert_axis_pos = self.my_joystick.get_axis(1)
         self.spawn_coins(coin_spawn_percent)
         self.player1.update((horiz_axis_pos, vert_axis_pos))
+        self.check_coin_collision()
         self.coins.update()
         self.surface.fill(background_color)
+        self.draw_score(self.surface)
         self.player.draw(self.surface)
         self.coins.draw(self.surface)
         if self.player1.check_bump(screen_width, screen_height):
